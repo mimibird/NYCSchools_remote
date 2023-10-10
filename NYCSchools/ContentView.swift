21c85cc5ba228b0e3b27a34630fb5fdfaacf14e8
yining <yiningzha.yz@gmail.com>
2023-10-10 11:37:44 -0400

    Initial Commit

---
+++ NYCSchools/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NYCSchools
+//
+//  Created by yining zha on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by yining zha on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
