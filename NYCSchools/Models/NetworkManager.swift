1a1cfe2533e1d9368334dab5f55898ebd1c0249f
yining <yiningzha.yz@gmail.com>
2023-10-14 21:42:47 -0400

    Renamed API constants for clarity.

--- NYCSchools/Models/NetworkManager.swift
+++ NYCSchools/Models/NetworkManager.swift
@@ -12,34 +12,34 @@ import Foundation
 class NetworkManager: ObservableObject {
     @Published var schools = [CombinedSchool]()
 
-    let url1 = URL(string: "https://data.cityofnewyork.us/resource/s3k6-pzi2.json")!
-    let url2 = URL(string: "https://data.cityofnewyork.us/resource/f9bf-2cp4.json")!
+    let getNycSchoolsUrl = URL(string: "https://data.cityofnewyork.us/resource/s3k6-pzi2.json")!
+    let getSatScoresUrl = URL(string: "https://data.cityofnewyork.us/resource/f9bf-2cp4.json")!
 
     func fetchData() {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
 
-        URLSession.shared.dataTask(with: url1) { data1, _, _ in
-            guard let data1 = data1, let schools1 = try? decoder.decode([CombinedSchool].self, from: data1) else {
-                print("Error decoding data from URL1")
+        URLSession.shared.dataTask(with: getNycSchoolsUrl) { nycSchoolsData, _, _ in
+            guard let nycSchoolsData = nycSchoolsData, let nycSchoolsList = try? decoder.decode([CombinedSchool].self, from: nycSchoolsData) else {
+                print("Error decoding data from NYC Schools URL")
                 return
             }
 
-            URLSession.shared.dataTask(with: self.url2) { data2, _, _ in
-                guard let data2 = data2, let schools2 = try? decoder.decode([CombinedSchool].self, from: data2) else {
-                    print("Error decoding data from URL2")
+            URLSession.shared.dataTask(with: self.getSatScoresUrl) { satScoresData, _, _ in
+                guard let satScoresData = satScoresData, let satScoresList = try? decoder.decode([CombinedSchool].self, from: satScoresData) else {
+                    print("Error decoding data from SAT Scores URL")
                     return
                 }
 
-                let combined = schools1.map { school1 -> CombinedSchool in
-                    if let matchedSchool2 = schools2.first(where: { $0.dbn == school1.dbn }) {
-                        return CombinedSchool(dbn: school1.dbn,
-                                              schoolName: school1.schoolName,
-                                              satMathAvgScore: matchedSchool2.satMathAvgScore,
-                                              satCriticalReadingAvgScore: matchedSchool2.satCriticalReadingAvgScore,
-                                              satWritingAvgScore: matchedSchool2.satWritingAvgScore)
+                let combined = nycSchoolsList.map { nycSchoolsList -> CombinedSchool in
+                    if let matchedSatScore = satScoresList.first(where: { $0.dbn == nycSchoolsList.dbn }) {
+                        return CombinedSchool(dbn: nycSchoolsList.dbn,
+                                              schoolName: nycSchoolsList.schoolName,
+                                              satMathAvgScore: matchedSatScore.satMathAvgScore,
+                                              satCriticalReadingAvgScore: matchedSatScore.satCriticalReadingAvgScore,
+                                              satWritingAvgScore: matchedSatScore.satWritingAvgScore)
                     }
-                    return school1
+                    return nycSchoolsList
                 }
 
                 DispatchQueue.main.async {
