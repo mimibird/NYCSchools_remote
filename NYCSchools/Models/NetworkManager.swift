ab30facc5360fc704178ce0449fa0f3672f5c35b
yining <yiningzha.yz@gmail.com>
2023-10-17 21:07:44 -0400

    Refactored NetworkManager.swift into  smaller functions.

--- NYCSchools/Models/NetworkManager.swift
+++ NYCSchools/Models/NetworkManager.swift
@@ -11,41 +11,52 @@ import Foundation
 
 class NetworkManager: ObservableObject {
     @Published var schools = [CombinedSchool]()
-
+    
     let getNycSchoolsUrl = URL(string: "https://data.cityofnewyork.us/resource/s3k6-pzi2.json")!
     let getSatScoresUrl = URL(string: "https://data.cityofnewyork.us/resource/f9bf-2cp4.json")!
-
+    
     func fetchData() {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-
-        URLSession.shared.dataTask(with: getNycSchoolsUrl) { nycSchoolsData, _, _ in
-            guard let nycSchoolsData = nycSchoolsData, let nycSchoolsList = try? decoder.decode([CombinedSchool].self, from: nycSchoolsData) else {
-                print("Error decoding data from NYC Schools URL")
-                return
-            }
-
-            URLSession.shared.dataTask(with: self.getSatScoresUrl) { satScoresData, _, _ in
-                guard let satScoresData = satScoresData, let satScoresList = try? decoder.decode([CombinedSchool].self, from: satScoresData) else {
-                    print("Error decoding data from SAT Scores URL")
-                    return
-                }
-
-                let combined = nycSchoolsList.map { nycSchoolsList -> CombinedSchool in
-                    if let matchedSatScore = satScoresList.first(where: { $0.dbn == nycSchoolsList.dbn }) {
-                        return CombinedSchool(dbn: nycSchoolsList.dbn,
-                                              schoolName: nycSchoolsList.schoolName,
-                                              satMathAvgScore: matchedSatScore.satMathAvgScore,
-                                              satCriticalReadingAvgScore: matchedSatScore.satCriticalReadingAvgScore,
-                                              satWritingAvgScore: matchedSatScore.satWritingAvgScore)
-                    }
-                    return nycSchoolsList
-                }
-
+        fetchNYCSchools { nycSchoolsList in
+            self.fetchSATScores { satScoresList in
+                let combined = self.combineData(nycSchools: nycSchoolsList, satScores: satScoresList)
                 DispatchQueue.main.async {
                     self.schools = combined
                 }
-            }.resume()
+            }
+        }
+    }
+    
+    private func fetchNYCSchools(completion: @escaping ([CombinedSchool]) -> Void) {
+        fetch(from: getNycSchoolsUrl, completion: completion)
+    }
+    
+    private func fetchSATScores(completion: @escaping ([CombinedSchool]) -> Void) {
+        fetch(from: getSatScoresUrl, completion: completion)
+    }
+    
+    private func fetch(from url: URL, completion: @escaping ([CombinedSchool]) -> Void) {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data, let schools = try? decoder.decode([CombinedSchool].self, from: data) else {
+                print("Error decoding data from \(url)")
+                return
+            }
+            completion(schools)
         }.resume()
+    }
+    
+    private func combineData(nycSchools: [CombinedSchool], satScores: [CombinedSchool]) -> [CombinedSchool] {
+        return nycSchools.map { nycSchool -> CombinedSchool in
+            if let matchedSatScore = satScores.first(where: { $0.dbn == nycSchool.dbn }) {
+                return CombinedSchool(dbn: nycSchool.dbn,
+                                      schoolName: nycSchool.schoolName,
+                                      satMathAvgScore: matchedSatScore.satMathAvgScore,
+                                      satCriticalReadingAvgScore: matchedSatScore.satCriticalReadingAvgScore,
+                                      satWritingAvgScore: matchedSatScore.satWritingAvgScore)
+            }
+            return nycSchool
+        }
     }
 }
